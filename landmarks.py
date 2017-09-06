29ceeab2a31aa3c24b4350ccb6c29d9d03726bdd
Straton Samaridis <straton.samaridis@cogni.zone>
2017-09-06 22:29:22 +0200

    try

--- landmarks.py
+++ landmarks.py
@@ -1,34 +1,33 @@
-import os,sys
+import os, sys
 import numpy as np
 
+
+
 def landmarks():
-    '''Specifing directories'''
+    # Specifying directories
     dirAll = sys.path[0] + '/_Data/Landmarks'
     dirOriginal = sys.path[0] + r"/_Data/Landmarks/original"
     dirMirrored = sys.path[0] + r"/_Data/Landmarks/mirrored"
 
     '''Declaring the 2 lists that will have the coordinates'''
-    landmarksOriginal = []
-    landmarksMirrored = []
+    landmarks = []
 
     '''Importing data'''
     for landmark in os.listdir(dirOriginal):
         textFile = open(os.path.join(dirOriginal, landmark), 'r')
         coordinates = np.reshape(textFile.read().splitlines(), (40, 2))
-        landmarksOriginal.append(coordinates)
-
-    '''Transforming the initial list for Original elements to array and then reshaping'''
-    landmarksOriginal = np.asarray(landmarksOriginal)
-    landmarksOriginal = np.reshape(landmarksOriginal, (14, 8, 40, 2)).astype(float)
+        landmarks.append(coordinates)
 
     for landmark in os.listdir(dirMirrored):
         textFile = open(os.path.join(dirMirrored, landmark), 'r')
         coordinates = np.reshape(textFile.read().splitlines(), (40, 2))
-        landmarksMirrored.append(coordinates)
+        landmarks.append(coordinates)
 
-    '''Transforming the initial list for Mirrored elements to array and then reshaping'''
-    landmarksMirrored = np.asarray(landmarksMirrored)
-    landmarksMirrored = np.reshape(landmarksMirrored, (14, 8, 40, 2)).astype(float)
+    '''Transforming the initial list for Original elements to array and then reshaping'''
+    landmarks = np.asarray(landmarks)
+    landmarks = np.reshape(landmarks, (28, 8, 40, 2)).astype(float)
 
     textFile.close()
-    return landmarksOriginal, landmarksMirrored
+    return landmarks
+
+print (landmarks())
---
+++ main.py
@@ -0,0 +1,6 @@
+import landmarks
+
+# here we call all functions
+
+#1.1
+landmarks=landmarks.landmarks()
